af05275192e1e10b1fd2bb346e79f7644214469a
daffahaidar <daffa.h.n.zufar@gmail.com>
2022-06-29 21:11:33 +0700

    Daffa Haidar - Customize Login Form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,48 +1,104 @@
-<x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
+<!DOCTYPE html>
+<html lang="en">
 
-        <x-jet-validation-errors class="mb-4" />
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../images/favicon.ico">
 
-        @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ session('status') }}
+    <title>Your Brand - Masuk </title>
+
+    <!-- Vendors Style-->
+    <link rel="stylesheet" href="{{ asset('backend/css/vendors_css.css') }}">
+
+    <!-- Style-->
+    <link rel="stylesheet" href="{{ asset('backend/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('backend/css/skin_color.css') }}">
+
+</head>
+
+<body class="hold-transition theme-primary bg-gradient-primary">
+
+    <div class="container h-p100">
+        <div class="row align-items-center justify-content-md-center h-p100">
+
+            <div class="col-12">
+                <div class="row justify-content-center no-gutters">
+                    <div class="col-lg-4 col-md-5 col-12">
+                        <div class="content-top-agile p-10">
+                            <h2 class="text-white">LOGIN</h2>
+                            <p class="text-white-50">School Management System</p>
+                        </div>
+                        <div class="p-30 rounded30 box-shadowed b-2 b-dashed">
+
+                            <form method="POST" action="{{ route('login') }}">
+                                @csrf
+
+                                <div class="form-group">
+                                    <div class="input-group mb-3">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text bg-transparent text-white"><i class="ti-email"></i></span>
+                                        </div>
+                                        <input type="email" id="email" name="email" class="form-control pl-15 bg-transparent text-white plc-white" placeholder="Email">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="input-group mb-3">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text  bg-transparent text-white"><i class="ti-lock"></i></span>
+                                        </div>
+                                        <input type="password" id="password" name="password" class="form-control pl-15 bg-transparent text-white plc-white" placeholder="Kata Sandi">
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-6">
+                                        <div class="checkbox text-white">
+                                            <input type="checkbox" id="basic_checkbox_1">
+                                            <label for="basic_checkbox_1">Ingat Saya</label>
+                                        </div>
+                                    </div>
+                                    <!-- /.col -->
+                                    <div class="col-6">
+                                        <div class="fog-pwd text-right">
+                                            <a href="{{ route('password.request') }}" class="text-white"><i class="ion ion-locked"></i> Lupa Sandi?</a><br>
+                                        </div>
+                                    </div>
+                                    <!-- /.col -->
+                                    <div class="col-12 text-center">
+                                        <button type="submit" class="btn btn-info btn-rounded mt-10">MASUK</button>
+                                    </div>
+                                    <!-- /.col -->
+                                </div>
+                            </form>
+
+                            <div class="text-center text-white">
+                                <p class="mt-20">- Masuk menggunakan -</p>
+                                <p class="gap-items-2 mb-20">
+                                    <a class="btn btn-social-icon btn-round btn-outline btn-white" href="#"><i class="fa fa-facebook"></i></a>
+                                    <a class="btn btn-social-icon btn-round btn-outline btn-white" href="#"><i class="fa fa-twitter"></i></a>
+                                    <a class="btn btn-social-icon btn-round btn-outline btn-white" href="#"><i class="fa fa-google-plus"></i></a>
+                                    <a class="btn btn-social-icon btn-round btn-outline btn-white" href="#"><i class="fa fa-instagram"></i></a>
+                                </p>
+                            </div>
+
+                            <div class="text-center">
+                                <p class="mt-15 mb-0 text-white">Belum punya akun? <a href="{{ route('register') }}" class="text-white ml-5">Daftar</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-        @endif
+        </div>
+    </div>
 
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
 
-            <div>
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            </div>
+    <!-- Vendor JS -->
+    <script src="{{ asset('backend/js/vendors.min.js') }}"></script>
+    <script src="{{ asset('../assets/icons/feather-icons/feather.min.js') }}"></script>
 
-            <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
-            </div>
+</body>
 
-            <div class="block mt-4">
-                <label for="remember_me" class="flex items-center">
-                    <x-jet-checkbox id="remember_me" name="remember" />
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-jet-button class="ml-4">
-                    {{ __('Log in') }}
-                </x-jet-button>
-            </div>
-        </form>
-    </x-jet-authentication-card>
-</x-guest-layout>
+</html>

64e71dc734a745a00f24977eeb1e73f1b4198cc3
daffahaidar <daffa.h.n.zufar@gmail.com>
2022-06-29 12:54:25 +0700

    Daffa Haidar - Dashboard Page Segmentation

---
+++ resources/views/admin/body/footer.blade.php
@@ -0,0 +1,13 @@
+<footer class="main-footer">
+    <div class="pull-right d-none d-sm-inline-block">
+        <ul class="nav nav-primary nav-dotted nav-dot-separated justify-content-center justify-content-md-end">
+            <li class="nav-item">
+                <a class="nav-link" href="javascript:void(0)">FAQ</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">www.yourbrand.com</a>
+            </li>
+        </ul>
+    </div>
+    &copy; 2020 - <?php echo $curr_year = date('Y'); ?><a href="#"> Your Brand</a> | All Rights Reserved.
+</footer>

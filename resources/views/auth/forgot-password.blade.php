ec5e92ca0621ab2795700e5c9bea65978993b43d
daffahaidar <daffa.h.n.zufar@gmail.com>
2022-06-30 09:35:08 +0700

    Daffa Haidar - Customize Forgot Password Form

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,34 +1,68 @@
-<x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
+<!DOCTYPE html>
+<html lang="en">
 
-        <div class="mb-4 text-sm text-gray-600">
-            {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../images/favicon.ico">
+
+    <title>Lupa Kata Sandi</title>
+
+    <!-- Vendors Style-->
+    <link rel="stylesheet" href="{{ asset('backend/css/vendors_css.css') }}">
+
+    <!-- Style-->
+    <link rel="stylesheet" href="{{ asset('backend/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('backend/css/skin_color.css') }}">
+
+</head>
+
+<body class="hold-transition theme-primary bg-gradient-primary">
+
+    <div class="container h-p100">
+        <div class="row align-items-center justify-content-md-center h-p100">
+
+            <div class="col-12">
+                <div class="row justify-content-center no-gutters">
+                    <div class="col-lg-4 col-md-5 col-12">
+                        <div class="content-top-agile p-10">
+                            <h3 class="mb-0 text-white">Lupa Kata Sandi?</h3>
+                        </div>
+                        <div class="p-30 rounded30 box-shadowed b-2 b-dashed">
+
+                            <form method="POST" action="{{ route('password.email') }}">
+                                @csrf
+                                <div class="form-group">
+                                    <div class="input-group mb-3">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text bg-transparent text-white"><i class="ti-email"></i></span>
+                                        </div>
+                                        <input type="email" id="email" name="email" :value="old('email')" class="form-control pl-15 bg-transparent text-white plc-white" placeholder="Masukan Email Anda" required autofocus>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-12 text-center">
+                                        <button type="submit" class="btn btn-info btn-rounded margin-top-10">Kirim Email</button>
+                                    </div>
+                                    <!-- /.col -->
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+    </div>
 
-        @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ session('status') }}
-            </div>
-        @endif
 
-        <x-jet-validation-errors class="mb-4" />
 
-        <form method="POST" action="{{ route('password.email') }}">
-            @csrf
+    <!-- Vendor JS -->
+    <script src="{{ asset('backend/js/vendors.min.js') }}"></script>
+    <script src="{{ asset('../assets/icons/feather-icons/feather.min.js') }}"></script>
 
-            <div class="block">
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            </div>
+</body>
 
-            <div class="flex items-center justify-end mt-4">
-                <x-jet-button>
-                    {{ __('Email Password Reset Link') }}
-                </x-jet-button>
-            </div>
-        </form>
-    </x-jet-authentication-card>
-</x-guest-layout>
+</html>
